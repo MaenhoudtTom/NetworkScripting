f2c1c45e79a60cb7a2be157ecabd8d4820b6886c
TomMaenhoudt <tom.maenhoudt@student.howest.be>
2021-09-29 11:58:45 +0200

    first attempt at NTFS

---
+++ MS/homeShare.ps1
@@ -0,0 +1,16 @@
+﻿#errors
+$domainCredential = "$env:USERDOMAIN\administrator"
+$computer = "Win06-MS"
+$path = "C:\Home"
+New-PSSession -ComputerName $computer -Credential $domainCredential
+Invoke-Command -ComputerName $computer -ScriptBlock {
+    Write-Host $env:COMPUTERNAME
+    New-Item -Path $path -ItemType Directory
+    New-SmbShare -Name "Home" -Path $path -FullAccess "Everyone"
+
+    $NewAcl = Get-Acl -Path $path
+    $isProtected = $true
+    $preserveInheritance = $true
+    $NewAcl.SetAccessRuleProtection($isProtected, $preserveInheritance)
+    Set-Acl -Path $path -AclObject $NewAcl
+}
