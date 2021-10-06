94711fab650633f32e70fc86fa8f3facd4316a97
TomMaenhoudt <tom.maenhoudt@student.howest.be>
2021-10-06 17:43:24 +0200

    Fixed User creation

--- DC/Users.ps1
+++ DC/Users.ps1
@@ -1,9 +1,9 @@
-﻿#errors
-$path = "C:\Users\Administrator\Documents\NetworkScripting\NetworkScripting\OUStructuur\UserAccounts.csv"
+﻿$path = "C:\Users\Administrator\Documents\NetworkScripting\NetworkScripting\OUStructuur\UserAccounts.csv"
 Write-Host $path
 $UserAccounts = Import-Csv -Path $path -Delimiter ";"
 
 Foreach ($user in $UserAccounts) {
+    $UPN = "mijnschool.be"
     $Name = $user.Name
     $SamAccountName = $user.SamAccountName
     $GivenName = $user.GivenName
@@ -11,23 +11,24 @@ Foreach ($user in $UserAccounts) {
     $DisplayName = $user.DisplayName
     $Pswd = ConvertTo-SecureString $user.AccountPassword -AsPlainText -Force
     $HomeDrive = $user.HomeDrive
-    $HomeDirectory = $user.HomeDirectory
+    $HomeDirectory = "\\win06-MS\Home\$Name"
     $ScriptPath = $user.ScriptPath
     $Path = $user.Path
     $HomeFolderPath = "\\Win06-MS\Home\$Name"
-    
+
+    New-ADUser -Name $Name -SamAccountName $SamAccountName -GivenName $GivenName -Surname $Surname -DisplayName $DisplayName -AccountPassword $Pswd -HomeDrive $HomeDrive -HomeDirectory $HomeDirectory -ScriptPath $ScriptPath -Path $Path -UserPrincipalName "$Name@$UPN"
+    Enable-ADAccount -Identity $Name
+    Write-Host "User $Name is created"
+
     New-Item -Path $HomeFolderPath -ItemType Directory
     Write-Host "Home folder for $Name created"
 
     $NewAcl = Get-Acl -Path $HomeFolderPath
-    $NewAcl.SetAccessRule($false, $true)
+    $NewAcl.SetAccessRuleProtection($false, $true)
     $accesRule = New-Object System.Security.AccessControl.FileSystemAccessRule($Name, "Modify", "ContainerInherit, objectInherit", "None", "Allow")
     Write-Host $accesRule
     $NewAcl.AddAccessRule($accesRule)
-    Write-Host "Home Folder for $Name created"
 
     Set-Acl -Path $HomeFolderPath -AclObject $NewAcl
-
-    #New-ADUser -Name $Name -SamAccountName $SamAccountName -GivenName $GivenName -Surname $Surname -DisplayName $DisplayName -AccountPassword $Pswd -HomeDrive $HomeDrive -HomeDirectory $HomeDirectory -ScriptPath $ScriptPath -Path $Path
-    Write-Host "User $Name is created"
+    Write-Host "Home Folder for $Name created"
 }
