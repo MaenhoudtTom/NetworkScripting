3424f78763baa9314ef9e2319b7a679347d3a25c
TomMaenhoudt <tom.maenhoudt@student.howest.be>
2021-10-06 09:52:25 +0200

    Fixed NTFS

--- MS/homeShare.ps1
+++ MS/homeShare.ps1
@@ -1,16 +1,23 @@
-﻿#errors
-$domainCredential = "$env:USERDOMAIN\administrator"
+﻿$domainCredential = "$env:USERDOMAIN\administrator"
 $computer = "Win06-MS"
-$path = "C:\Home"
+
 New-PSSession -ComputerName $computer -Credential $domainCredential
 Invoke-Command -ComputerName $computer -ScriptBlock {
+    $path = "C:\Home"
     Write-Host $env:COMPUTERNAME
+    Write-Host $path
     New-Item -Path $path -ItemType Directory
     New-SmbShare -Name "Home" -Path $path -FullAccess "Everyone"
 
     $NewAcl = Get-Acl -Path $path
     $isProtected = $true
-    $preserveInheritance = $true
+    $preserveInheritance = $false
     $NewAcl.SetAccessRuleProtection($isProtected, $preserveInheritance)
+
+    $accesRule = New-Object System.Security.AccessControl.FileSystemAccessRule("Administrators", "FullControl", "ContainerInherit, objectInherit", "None", "Allow")
+    $NewAcl.AddAccessRule($accesRule)
+    $accesRule = New-Object System.Security.AccessControl.FileSystemAccessRule("Authenticated Users", "ReadAndExecute", "None", "None", "Allow")
+    $NewAcl.AddAccessRule($accesRule)
+
     Set-Acl -Path $path -AclObject $NewAcl
 }
