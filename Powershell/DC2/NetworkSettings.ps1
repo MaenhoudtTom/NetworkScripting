555e463afa7afc44bf963dd7dba27399b9b9e4d0
TomMaenhoudt <tom.maenhoudt@student.howest.be>
2021-10-13 09:26:17 +0200

    Added DC2 folder and adjusted folder structure

---
+++ Powershell/DC2/NetworkSettings.ps1
@@ -0,0 +1,29 @@
+﻿# Hostname
+#Rename-Computer -NewName "DC1"
+
+# Static IP
+$IP = "192.168.1.2"
+$Subnet = 24
+$gateway = "192.168.1.1"
+$DNS = "172.20.0.2"
+$IPType = "IPv4"
+
+# get adapter
+$adapter = Get-NetAdapter -Physical | Where-Object { $_.physicalMediatype -match "802.3" -and $_.status -eq "up"}
+
+# Remove any existing IP, gateway from our ipv4 adapter
+If (($adapter | Get-NetIPConfiguration).IPv4Address.IPAddress) {
+ $adapter | Remove-NetIPAddress -AddressFamily $IPType -Confirm:$false
+}
+If (($adapter | Get-NetIPConfiguration).Ipv4DefaultGateway) {
+ $adapter | Remove-NetRoute -AddressFamily $IPType -Confirm:$false
+}
+
+# Set new IP
+$adapter | New-NetIPAddress -AddressFamily $IPType -IPAddress $IP -PrefixLength $Subnet -DefaultGateway $gateway
+
+#Configure DNS
+$adapter | Set-DnsClientServerAddress -ServerAddresses $DNS
+
+#restart computer
+Restart-Computer
---
+++ Powershell/DC2/Promote to DC.ps1
@@ -0,0 +1,4 @@
+﻿$Domain = "intranet.mijnschool.be"
+# Install AD services
+Install-WindowsFeature -Name AD-Domain-Services -IncludeManagementTools
+Install-ADDSForest -DomainName $Domain -InstallDNS
