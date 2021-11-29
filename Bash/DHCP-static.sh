84190e8d19574487e02ebc25848ee499852306c5
TomMaenhoudt <tom.maenhoudt@student.howest.be>
2021-11-29 12:30:40 +0100

    First try at DHCP/Static script

---
+++ Bash/DHCP-static.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+
+# Variables
+interface="ens32"
+ipaddress="191.168.214.240/24"
+gateway="191.168.214.236"
+
+# Check if the script is run as root
+if [ "$(id -u)" -ne 0 ]
+then
+	# Stopping the script
+	echo "This script must be run as root"
+	exit 1
+fi
+
+# Check for parameter
+if [ -z "$1" ]
+then
+	# Stopping the script
+	echo "Please run the script with one of these parameters: dchp, static"
+	echo "Exmple: sudo sh DHCP-static.sh dhcp"
+	exit 2
+fi
+
+set_dhcp() {
+	sed -e "s,auto $interface\niface $interface static\naddress $ipaddress\ngateway $gateway,iface $interface inet dhcp,g" /etc/network/interfaces
+	
+	echo "-----------------"
+	echo "DHCP set"
+	echo "-----------------"
+
+	eval "systemctl restart networking.service"
+	echo ""
+	eval "ip a"
+}
+
+set_staic() {
+	sed -e "s,iface $interface inet dhcp, auto $interface\niface $interface static\naddress $ipaddress\ngateway $gateway,g" /etc/network/interfaces
+
+	echo "-----------------"
+	echo "Static IP set"
+	echo "-----------------"
+
+	eval "systemctl restart networking.service"
+	echo ""
+	eval "ip a"
+}
+
+# Check parameter and execute function
+echo "Param 1 is: $1"
+if [ "$1"="dhcp" ]
+then
+	set_dhcp
+elif [ "$1"="static" ]
+then
+	set_static
+else
+	echo "Wrong parameter: $1"
+	echo "The parameters are 'dhcp' and 'static'"
+	exit 3
+fi
