0070f19d08be2bbe39d5d7e07345597243613143
TomMaenhoudt <tom.maenhoudt@student.howest.be>
2021-12-22 11:07:32 +0100

    Added first script part of part3
    Also testing if git works because of conflict errors

---
+++ Examen/Deel3/generateCommands.ps1
@@ -0,0 +1,17 @@
+﻿$pathToFile = "D:\Tom\MCT\Semester_5\NetworkScripting\Examen\Deel3\LVL-2-POE-config.txt"
+
+
+
+$nameFile = "$hostname-$ipVlan1.txt"
+$pathToDestinationFile = "D:\Tom\MCT\Semester_5\NetworkScripting\Examen\Deel3\$nameFile"
+
+foreach($line in [System.IO.File]::ReadLines($pathToFile))
+{
+    $array = $line.Split(" ");
+    "vlan 999" >> $pathToDestinationFile
+    "name VL.999" >> $pathToDestinationFile
+    "interface vlan999" >> $pathToDestinationFile
+    "description VL.999" >> $pathToDestinationFile
+    'no ip address' >> $pathToDestinationFile
+    'no shut' >> $pathToDestinationFile
+}
